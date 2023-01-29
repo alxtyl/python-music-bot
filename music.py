432f261a7b4acbdaef6e2c13171983eb16b4000e
alxtyl <bip760@icloud.com>
2023-01-28 23:02:27 -0800

    Update formatting for sys info cmd

--- music.py
+++ music.py
@@ -205,9 +205,9 @@ class MusicBot(commands.Cog):
         py_ver = subprocess.check_output('python3 --version', shell=True).decode('utf-8').strip()
         sys_info = subprocess.check_output('lsb_release -d', shell=True).decode('utf-8').split('\t')[1].strip()
 
-        embed = discord.Embed(title="System Info", color=discord.Color.dark_blue())
+        embed = discord.Embed(title="Currently running:", color=discord.Color.og_blurple())
         info_lst = '\n'.join([py_ver, sys_info])  # Joining the list with newline as the delimiter
-        embed.add_field(name="Specs", value=info_lst)
+        embed.add_field(name="System info", value=info_lst)
         return await ctx.send(embed=embed)
 
 async def setup(bot):
