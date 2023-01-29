c7dea7ff721f201e793709eb8caf2d62d11d4f33
alxtyl <bip760@icloud>
2023-01-29 15:10:20 -0800

    Add in reason param

--- music.py
+++ music.py
@@ -37,7 +37,7 @@ class MusicBot(commands.Cog):
         logging.info(f"{node} is ready")
 
     @commands.Cog.listener()
-    async def on_wavelink_track_end(self, player: wavelink.Player, track: wavelink.Track):
+    async def on_wavelink_track_end(self, player: wavelink.Player, track: wavelink.Track, reason):
         ctx = player.ctx
         vc: player = ctx.voice_client
 
