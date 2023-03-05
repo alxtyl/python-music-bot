60e769fb7a2fa0da8e004d61d724ce3ceb7b09e5
alxtyl <bip760@icloud.com>
2023-03-05 22:49:26 +0000

    Added cogs folder

--- main.py
+++ main.py
@@ -5,23 +5,23 @@ Currently working with Lavalink v3.5.1
 
 import os
 import discord
-from discord.ext import commands
 import logging
+from discord.ext import commands
 
 logging.basicConfig(level=logging.DEBUG)
 
 def run():
+    cogs = ["cogs.music", "cogs.misc"]
     intents = discord.Intents.all()
-
     bot = commands.Bot(command_prefix='!', intents=intents)
 
     @bot.event
     async def on_ready():
         await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name='for !'))
-        await bot.load_extension("music")
-        await bot.load_extension("misc")
+        for cog in cogs:
+            await bot.load_extension(cog)
 
-    bot.run(os.environ['BOT_KEY'], root_logger=True)
+    bot.run(os.environ['BOT_KEY'], reconnect=True, root_logger=True)
 
 if __name__ == "__main__":
     run()
